0b32394b0bea23f935a7bac1747984a52a879f56
Taylor Schofield <tayrannosaur@protonmail.com>
2020-01-05 12:47:07 -0700

    Configure prefix from env
    Added logic to import command prefix from an environment variable called
    $SPOONBOT_PREFIX. The purpose of this is to make deploying different
    instances of SpoonBot to the same server easier as it will help prevent
    two instances from having the same command prefix.

--- spoonbot/spoonbot.py
+++ spoonbot/spoonbot.py
@@ -7,18 +7,29 @@ import dotenv
 from .util import Util
 from .fun  import Fun
 
-def launch():
+
+def getToken():
     try:
-        TOKEN = os.environ['SPOONBOT_TOKEN']
+        return os.environ['SPOONBOT_TOKEN']
     except KeyError:
         print("Could not launch: Didn't find a login token!")
 
+def getPrefix():
+    try:
+        return os.environ['SPOONBOT_PREFIX']
+    except KeyError:
+        print("Could not launch: Didn't find a command prefix!")
+
+def launch():
+    token = getToken()
+    prefix = getPrefix()
+
     description = '''This is a custom bot for Clara_The_Classy's Discord server!'''
 
-    bot = commands.Bot(command_prefix='~', description=description)
+    bot = commands.Bot(command_prefix=prefix, description=description)
 
     bot.add_cog(Util(bot))
     bot.add_cog(Fun(bot))
 
 
-    bot.run(TOKEN)
+    bot.run(token)
